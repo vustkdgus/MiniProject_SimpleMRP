0c0c6efb4b2c269293f47ed2e9226d23242f051f
vustkdgus <vustkdgus@naver.com>
2021-06-24 17:28:01 +0900

    2021.06.24 공정 계획화면 개발중

---
+++ MRPApp/MRPApp.Test/SettingsTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MRPApp.View.Setting;
+using System;
+using System.Linq;
+
+namespace MRPApp.Test
+{
+    [TestClass]
+    public class SettingsTest
+    {
+        // Db사에 중복된 데이터 있는지 테스트
+        [TestMethod]
+        public void IsDuplicateDataTest()
+        {
+            var expectVal = true;  // 예상값
+            var inputCode = "PC010001";  // DB 실제값
+
+            var code = Logic.DataAccess.GetSettings().Where(d => d.BasicCode.Contains(inputCode)).FirstOrDefault();
+            var realVal = code != null ? true : false;
+
+            Assert.AreNotEqual(expectVal, realVal); // 값이 같으면 Pass, 다르면 Fail
+        }
+    }
+}
--- MRPApp/MRPApp/Logic/DataAccess.cs
+++ MRPApp/MRPApp/Logic/DataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MRPApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
@@ -9,6 +10,35 @@ namespace MRPApp.Logic
 {
     public class DataAccess
     {
-        
+        // 셋팅테이블에서 데이터 가져오기
+        public static List<Settings> GetSettings()
+        {
+            List<Model.Settings> settings;
+
+            using (var ctx = new MRPIEntities())
+                settings = ctx.Settings.ToList(); // SELECT
+
+            return settings;
+        }
+
+        internal static int SetSettings(Settings item)
+        {
+            using (var ctx = new MRPIEntities())
+            {
+                ctx.Settings.AddOrUpdate(item);
+                return ctx.SaveChanges(); // 데이터 커밋
+            }
+
+        }
+
+        internal static int DelSettings(Settings item)
+        {
+            using (var ctx = new MRPIEntities())
+            {
+                var obj = ctx.Settings.Find(item.BasicCode);
+                ctx.Settings.Remove(obj);
+                return ctx.SaveChanges(); // 데이터 커밋
+            }
+        }
     }
 }
